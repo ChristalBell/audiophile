74f4d0f932dd185bc418e8a56b6a63779fded14c
Christal Bell <christalvictoriabell@gmail.com>
2024-04-28 17:35:30 -0700

    cart preview styled for mobile

--- src/components/checkout/CartPreview.js
+++ src/components/checkout/CartPreview.js
@@ -1,7 +1,5 @@
 import React from "react";
 import data from "../../data.json";
-
-import "./preview.css";
 import ClearCartButton from "../buttons/ClearCartButton";
 import CheckoutButton from "../buttons/CheckoutButton";
 import CartItemCounter from "./CartItemCounter";
@@ -27,54 +25,64 @@ const CartPreview = () => {
 
   return (
     <div
-      className="invisible"
+      className="container"
       style={{
         backgroundColor: "white",
         borderRadius: ".25rem",
-        color: "black",
-        padding: "1rem",
+        boxShadow: "1rem .5rem .75rem hsl(0, 0%, 10%)",
+        color: "hsl(0, 0%, 5%)",
+        padding: "1rem 2rem ",
+        translate: "-4rem 0",
       }}
     >
-      <div style={{ display: "flex", alignItems: "center" }}>
+      <div className="top" style={{ display: "flex" }}>
         <p style={{ marginRight: "2rem" }}>CART ({cartCount})</p>
         <ClearCartButton />
       </div>
 
-      <div style={{ display: "flex" }}>
+      <div className="item-display">
         {/* {cartItems} */}
         {/* <img src={item.image.mobile} alt={item.name} /> */}
-
-        <Button
-          onClick={() => {
-            dispatch(lowerCartCount());
-            dispatch(minusOne());
-          }}
-        >
-          -
-        </Button>
-        <p>{cartCount}</p>
-        <Button
-          onClick={() => {
-            dispatch(raiseCartCount());
-            dispatch(addOne());
-          }}
-        >
-          +
-        </Button>
+        <div className="button" style={{ display: "flex" }}>
+          <Button
+            onClick={() => {
+              dispatch(lowerCartCount());
+              dispatch(minusOne());
+            }}
+          >
+            -
+          </Button>
+          <p>{cartCount}</p>
+          <Button
+            onClick={() => {
+              dispatch(raiseCartCount());
+              dispatch(addOne());
+            }}
+          >
+            +
+          </Button>
+        </div>
       </div>
 
       <div
+        className="total"
         style={{
           display: "flex",
+          flexDirection: "column",
           fontWeight: "bold",
           textTransform: "uppercase",
         }}
       >
-        <p>total</p>
-        <p>{data[0].price * cartCount}</p>
-      </div>
-      <div>
-        <CheckoutButton />
+        <div
+          className="price"
+          style={{ display: "flex", justifyContent: "space-between" }}
+        >
+          <p>total</p>
+          <p>{data[0].price * cartCount}</p>
+        </div>
+        <div style={{ alignSelf: "center" }}>
+          <CheckoutButton />
+        </div>
       </div>
     </div>
   );
