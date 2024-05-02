5f838ed0b620a4eef7e93f6f62588ab6eb8844a0
Christal Bell <christalvictoriabell@gmail.com>
2024-05-02 16:56:48 -0700

    trying to add persist, not working

--- src/index.js
+++ src/index.js
@@ -6,13 +6,19 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+let persistor = persistStore(store);
+
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
